7c6b07682ea9c37db48612c12e131d92be187ec3
AbhishekG-Codes <166795493+AbhishekG-Codes@users.noreply.github.com>
2025-03-15 12:08:01 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+import axios from "axios"
+
+export default function App() {
+  const [username,setUsername] = useState("")
+
+  async function handleSubmit(e){
+    e.preventDefault()
+    const response = await axios.post("http://localhost:3000/login",{username})
+    setUsername(response.data)
+    console.log(response.data)
+  }
+  
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" onChange={(e)=>{setUsername(e.target.value)}}/>
+      <input type="submit" />
+    </form>
+
+  )
+}
